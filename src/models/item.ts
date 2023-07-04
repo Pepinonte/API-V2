4109cd80645a769b6631d39590af49855b265b68
USPaTaTa <clem.met@hotmail.fr>
2023-07-04 16:21:13 +0200

    models for user + ItemTypes

--- src/models/item.ts
+++ src/models/item.ts
@@ -27,6 +27,10 @@ const Item = db.define(
       allowNull: true,
       defaultValue: null,
     },
+    item_author: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
     item_file: {
       type: DataTypes.STRING(100),
       allowNull: false,
@@ -35,6 +39,10 @@ const Item = db.define(
       type: DataTypes.INTEGER.UNSIGNED,
       allowNull: false,
     },
+    item_date_upload: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    }
   },
   {
     freezeTableName: true,
