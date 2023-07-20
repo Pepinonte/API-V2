b9017185e849c770053ac23103ef464efd524b48
Clément <clem.met@hotmail.fr>
2023-07-20 22:25:25 +0200

    auth deleted

--- src/routes/user/get.routes.ts
+++ src/routes/user/get.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import auth from "../../middlewares/auth";
+// import auth from "../../middlewares/auth";
 import * as getControllers from "../../controllers/user/get.controllers";
 
 const router = Router();
 
-router.get("/getAll/user", auth, getControllers.getAll);
+router.get("/getAll/user", getControllers.getAll);
 router.get("/getById/user/:id", getControllers.getById);
 router.get("/getByName/user/:name", getControllers.getByName);
 
